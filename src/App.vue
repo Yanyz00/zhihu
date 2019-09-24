973374cde8c5bb963976fdc4c5c67500ba6aff96
yyz <568627098@qq.com>
2019-09-24 12:33:51 +0800

    修改1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome 哈哈哈哈哈哈哈哈修改了"/>
   </div>
 </template>
 
