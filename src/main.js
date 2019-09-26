34f570b9400e9c974251bd2c63f5e67152a0efff
yz <568627098@qq.com>
2019-09-27 01:45:13 +0800

    'tabbar'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'common/config.js' // eslint-disable-line no-unused-vars
+import router from './router'
 Vue.config.productionTip = false
 new Vue({ 
   render: h => h(App),
+  router
 }).$mount('#app')
